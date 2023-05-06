3f30260c5aab2999d60ead56540fb7afe78849e5
Denis Novikov <alpden550@gmail.com>
2023-05-06 11:36:40 +0300

    feat(models): add creating transaction method

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type TransactionStatus struct {
 	ID        int       `json:"id"`
@@ -18,4 +21,25 @@ type Transaction struct {
 	TransactionStatusID int       `json:"transaction_status_id"`
 	CreatedAt           time.Time `json:"-"`
 	UpdatedAt           time.Time `json:"-"`
+}
+
+func (m *DBModel) InsertTransaction(t Transaction) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO transactions (amount, currency, last_four, bank_return_code, transaction_status_id)
+		VALUES ($1, $2, $3, $4, $5)
+	`
+
+	res, err := m.DB.ExecContext(ctx, query, t.Amount, t.Currency, t.LastFour, t.BankReturnCode, t.TransactionStatusID)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
