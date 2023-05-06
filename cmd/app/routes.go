730d59e05fa7b8cfc16649e6ed0dee0739dca3ce
Denis Novikov <alpden550@gmail.com>
2023-05-06 17:53:11 +0300

    feat(routes): add home page handler

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -8,10 +8,12 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(SessionLoad)
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
+	mux.Get("/", app.IndexPage)
 	mux.Get("/widgets/{id}", app.ChargeOnce)
 
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
