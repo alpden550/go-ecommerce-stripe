a1c6f9aa1421c2e5c0515b7e65a333c7aecd96d7
Denis Novikov <alpden550@gmail.com>
2023-05-09 16:36:07 +0300

    feat(models): fixing inserting transaction method

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"context"
 	"time"
 )
 
@@ -17,29 +16,50 @@ type Transaction struct {
 	Amount              int       `json:"amount"`
 	Currency            string    `json:"currency"`
 	LastFour            string    `json:"last_four"`
+	ExpireMonth         int       `json:"expire_month"`
+	ExpireYear          int       `json:"expire_year"`
 	BankReturnCode      string    `json:"bank_return_code"`
+	PaymentMethodCode   string    `json:"payment_method_code"`
+	PaymentIntentCode   string    `json:"payment_intent_code"`
 	TransactionStatusID int       `json:"transaction_status_id"`
 	CreatedAt           time.Time `json:"-"`
 	UpdatedAt           time.Time `json:"-"`
 }
 
 func (m *DBModel) InsertTransaction(t Transaction) (int, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+	var id int
 
 	query := `
-		INSERT INTO transactions (amount, currency, last_four, bank_return_code, transaction_status_id)
-		VALUES ($1, $2, $3, $4, $5)
+		INSERT INTO transactions 
+		    (amount,
+		     currency,
+		     last_four,
+		     bank_return_code,
+		     transaction_status_id,
+		     expire_month,
+		     expire_year,
+		     payment_method_code,
+		     payment_intent_code
+		     )
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+		RETURNING id
 	`
 
-	res, err := m.DB.ExecContext(ctx, query, t.Amount, t.Currency, t.LastFour, t.BankReturnCode, t.TransactionStatusID)
+	err := m.DB.QueryRow(
+		query,
+		t.Amount,
+		t.Currency,
+		t.LastFour,
+		t.BankReturnCode,
+		t.TransactionStatusID,
+		t.ExpireMonth,
+		t.ExpireYear,
+		t.PaymentMethodCode,
+		t.PaymentIntentCode,
+	).Scan(&id)
 	if err != nil {
 		return 0, err
 	}
 
-	id, err := res.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-	return int(id), nil
+	return id, nil
 }
