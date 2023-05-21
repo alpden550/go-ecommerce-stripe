f4135f5435683e529abfdd52c99a4718c36990ef
Denis Novikov <alpden550@gmail.com>
2023-05-21 13:36:01 +0300

    feat(models): add get widgets from db

--- internal/models/widget.go
+++ internal/models/widget.go
@@ -39,3 +39,37 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 
 	return widget, nil
 }
+
+func (m *DBModel) GetAllWidgets() ([]Widget, error) {
+	var widgets []Widget
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `SELECT id, name, description, price, inventory_level, coalesce(image, ''), created_at FROM widgets`
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var widget Widget
+		if err := rows.Scan(
+			&widget.ID,
+			&widget.Name,
+			&widget.Description,
+			&widget.Price,
+			&widget.InventoryLevel,
+			&widget.Image,
+			&widget.CreatedAt,
+		); err != nil {
+			return widgets, err
+		}
+		widgets = append(widgets, widget)
+	}
+
+	if err = rows.Err(); err != nil {
+		return widgets, err
+	}
+
+	return widgets, nil
+}
