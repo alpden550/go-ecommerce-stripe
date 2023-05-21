77db503d1531f1429989b7e570645f9802a823ee
Denis Novikov <alpden550@gmail.com>
2023-05-21 15:57:26 +0300

    feat(models): add get user by email from db

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"context"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        int       `json:"id"`
@@ -10,4 +14,24 @@ type User struct {
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
+}
+
+func (m *DBModel) GetUserByEmail(email string) (User, error) {
+	var user User
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	email = strings.ToLower(email)
+	query := `SELECT id, email, first_name, last_name, last_name, password  FROM users WHERE email=$1`
+	if err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+	); err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
