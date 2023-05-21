edf55c5fb77195a35fb4838949e792d639995cc5
Denis Novikov <alpden550@gmail.com>
2023-05-21 14:06:39 +0300

    refact(routes): use handlers name space

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	handlers2 "go-ecommerce/internal/handlers"
+	"go-ecommerce/internal/handlers"
 	"net/http"
 )
 
@@ -13,18 +13,18 @@ func routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
-	mux.Get("/", handlers2.IndexPage)
+	mux.Get("/", handlers.IndexPage)
 
-	mux.Get("/virtual-terminal", handlers2.VirtualTerminal)
-	mux.Post("/virtual-terminal/payment-succeeded", handlers2.VirtualTerminalPaymentSucceed)
-	mux.Get("/virtual-terminal/receipt", handlers2.VirtualTerminalShowReceipt)
+	mux.Get("/virtual-terminal", handlers.VirtualTerminal)
+	mux.Post("/virtual-terminal/payment-succeeded", handlers.VirtualTerminalPaymentSucceed)
+	mux.Get("/virtual-terminal/receipt", handlers.VirtualTerminalShowReceipt)
 
-	mux.Get("/widgets/{id}", handlers2.WidgetChargeOnce)
-	mux.Post("/widgets/payment-succeeded", handlers2.WidgetPaymentSucceed)
-	mux.Get("/widgets/receipt", handlers2.WidgetShowReceipt)
+	mux.Get("/widgets/{id}", handlers.WidgetChargeOnce)
+	mux.Post("/widgets/payment-succeeded", handlers.WidgetPaymentSucceed)
+	mux.Get("/widgets/receipt", handlers.WidgetShowReceipt)
 
-	mux.Get("/plans/bronze", handlers2.BronzePlan)
-	mux.Get("/plans/bronze/receipt", handlers2.BronzePlanShowReceipt)
+	mux.Get("/plans/bronze", handlers.BronzePlan)
+	mux.Get("/plans/bronze/receipt", handlers.BronzePlanShowReceipt)
 
 	return mux
 }
