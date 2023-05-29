15aaadcc4abf9efe727cfbe4b7290e46541f6df0
Denis Novikov <alpden550@gmail.com>
2023-05-29 15:17:21 +0300

    feat(handlers): add auth handlers

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -13,3 +13,32 @@ func Login(writer http.ResponseWriter, request *http.Request) {
 		app.ErrorLog.Printf("%e", fmt.Errorf("%w", err))
 	}
 }
+
+func SubmitLogin(writer http.ResponseWriter, request *http.Request) {
+	if err := app.Session.RenewToken(request.Context()); err != nil {
+		app.ErrorLog.Printf("%e", fmt.Errorf("%w", err))
+		return
+	}
+
+	if err := request.ParseForm(); err != nil {
+		app.ErrorLog.Printf("%e", fmt.Errorf("%w", err))
+		return
+	}
+
+	email, password := request.Form.Get("email"), request.Form.Get("password")
+	id, err := app.DB.Authenticate(email, password)
+	if err != nil {
+		http.Redirect(writer, request, "/auth/login", http.StatusSeeOther)
+		return
+	}
+
+	app.Session.Put(request.Context(), "userId", id)
+	http.Redirect(writer, request, "/", http.StatusSeeOther)
+}
+
+func Logout(writer http.ResponseWriter, request *http.Request) {
+	app.Session.Destroy(request.Context())
+	app.Session.RenewToken(request.Context())
+
+	http.Redirect(writer, request, "/", http.StatusSeeOther)
+}
