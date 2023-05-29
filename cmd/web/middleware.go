418d85de883e94019130c67f4bd3811a74929316
Denis Novikov <alpden550@gmail.com>
2023-05-29 15:18:29 +0300

    feat(middleware): add auth middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func MiddlewareAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if !app.Session.Exists(request.Context(), "userId") {
+			http.Redirect(writer, request, "/auth/login", http.StatusTemporaryRedirect)
+			return
+		}
+		next.ServeHTTP(writer, request)
+	})
 }
