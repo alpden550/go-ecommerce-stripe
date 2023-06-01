c019cac1678ed6ec2a773781ab1fbc267e4c2aa4
Denis Novikov <alpden550@gmail.com>
2023-06-01 15:46:24 +0300

    chore(migrations): style

--- migrations/20230529120120_create_session.postgres.up.sql
+++ migrations/20230529120120_create_session.postgres.up.sql
@@ -1,7 +1,8 @@
-CREATE TABLE sessions (
-                          token CHAR(43) PRIMARY KEY,
-                          data BYTEA NOT NULL,
-                          expiry TIMESTAMPTZ NOT NULL
+CREATE TABLE sessions
+(
+    token  CHAR(43) PRIMARY KEY,
+    data   BYTEA       NOT NULL,
+    expiry TIMESTAMPTZ NOT NULL
 );
 
 CREATE INDEX sessions_expiry_idx ON sessions (expiry);
