7097be39b30c203c32babb9f34bb2c53590140d9
Denis Novikov <alpden550@gmail.com>
2023-06-03 20:21:16 +0300

    feat(mailer): add send emails

---
+++ internal/mailer/mailer.go
@@ -0,0 +1,65 @@
+package mailer
+
+import (
+	"fmt"
+	"github.com/alpden550/go-ecommerce-stripe/internal/configs"
+	mail "github.com/xhit/go-simple-mail/v2"
+	"time"
+)
+
+func serveSMTPClient(api *configs.ApiApplication) (*mail.SMTPClient, error) {
+	mailer := mail.NewSMTPClient()
+	mailer.Host = api.Config.SMTP.Host
+	mailer.Port = api.Config.SMTP.Port
+	mailer.Username = api.Config.SMTP.Username
+	mailer.Password = api.Config.SMTP.Password
+	mailer.Encryption = mail.EncryptionTLS
+	mailer.KeepAlive = false
+	mailer.ConnectTimeout = 10 * time.Second
+	mailer.SendTimeout = 10 * time.Second
+
+	smtpClient, err := mailer.Connect()
+	if err != nil {
+		api.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		return nil, err
+	}
+
+	return smtpClient, nil
+}
+
+func SendEmail(api *configs.ApiApplication, from, to, subject, tmpl string, data interface{}) error {
+	formattedMessage, err := renderHTML(tmpl, data)
+	if err != nil {
+		api.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		return err
+	}
+
+	plainMessage, err := renderPlainText(tmpl, data)
+	if err != nil {
+		api.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		return err
+	}
+
+	smtp, err := serveSMTPClient(api)
+	if err != nil {
+		api.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(from).
+		AddTo(to).
+		SetSubject(subject)
+
+	email.SetBody(mail.TextHTML, formattedMessage)
+	email.AddAlternative(mail.TextPlain, plainMessage)
+
+	if err = email.Send(smtp); err != nil {
+		api.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		return err
+	}
+
+	fmt.Println(formattedMessage)
+
+	return nil
+}
