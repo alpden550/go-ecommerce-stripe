fa31fcb3028007f2775056eeb4eca5adf84e592c
Denis Novikov <alpden550@gmail.com>
2023-06-04 15:13:49 +0300

    fear(handlers): add sending reset password email

--- internal/handlers_api/auth.go
+++ internal/handlers_api/auth.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"github.com/alpden550/go-ecommerce-stripe/internal/helpers"
+	"github.com/alpden550/go-ecommerce-stripe/internal/mailer"
 	"github.com/alpden550/go-ecommerce-stripe/internal/models"
+	"github.com/alpden550/go-ecommerce-stripe/internal/urlsigner"
 	"net/http"
 	"strings"
 	"time"
@@ -95,4 +97,52 @@ func AuthenticateToken(request *http.Request) (*models.User, error) {
 	}
 
 	return user, nil
+}
+
+func SendPasswordResetEmail(writer http.ResponseWriter, request *http.Request) {
+	var payload struct {
+		Email string `json:"email"`
+	}
+
+	if err := helpers.ReadJSON(api, writer, request, &payload); err != nil {
+		_ = helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	if _, err := helpers.FetchUserByEmail(api, payload.Email); err != nil {
+		response := jsonResponse{
+			OK:      false,
+			Message: "no matching email found",
+		}
+		_ = helpers.WriteJSON(api, writer, http.StatusAccepted, response)
+		return
+	}
+
+	link := fmt.Sprintf("%s/auth/reset-password?email=%s", api.Config.FrontEnd, payload.Email)
+	signer := urlsigner.Signer{Secret: []byte(api.Config.SecretKey)}
+	signedLink := signer.GenerateTokenFromString(link)
+
+	var data struct {
+		Link string
+	}
+	data.Link = signedLink
+
+	if err := mailer.SendEmail(
+		api,
+		api.Config.SMTP.EmailFrom,
+		payload.Email,
+		"Password Reset Requested",
+		"password-reset.plain.tmpl",
+		"password-reset.html.tmpl",
+		data,
+	); err != nil {
+		api.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		_ = helpers.BadRequest(api, writer, request, err)
+	}
+
+	response := jsonResponse{
+		OK:      true,
+		Message: "sent",
+	}
+	_ = helpers.WriteJSON(api, writer, http.StatusOK, response)
 }
