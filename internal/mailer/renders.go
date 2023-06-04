0242ca405ce1067a6109c53486aced7d8e2d3f16
Denis Novikov <alpden550@gmail.com>
2023-06-04 15:05:28 +0300

    refact(renders): add base render template function

--- internal/mailer/renders.go
+++ internal/mailer/renders.go
@@ -10,23 +10,8 @@ import (
 //go:embed templates
 var emailTemplateFS embed.FS
 
-func renderHTML(tmpl string, data interface{}) (string, error) {
-	templateToRender := fmt.Sprintf("templates/%s.html.tmpl", tmpl)
-	t, err := template.New("email-html").ParseFS(emailTemplateFS, templateToRender)
-	if err != nil {
-		return "", err
-	}
-
-	var tpl bytes.Buffer
-	if err = t.ExecuteTemplate(&tpl, "body", data); err != nil {
-		return "", err
-	}
-	formattedMessage := tpl.String()
-	return formattedMessage, nil
-}
-
-func renderPlainText(tmpl string, data interface{}) (string, error) {
-	templateToRender := fmt.Sprintf("templates/%s.plain.tmpl", tmpl)
+func renderTemplate(tmpl string, data interface{}) (string, error) {
+	templateToRender := fmt.Sprintf("templates/%s", tmpl)
 	t, err := template.New("email-html").ParseFS(emailTemplateFS, templateToRender)
 	if err != nil {
 		return "", err
