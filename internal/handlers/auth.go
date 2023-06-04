a4a5df35c31316e024f85362be01ac99f97ec7dc
Denis Novikov <alpden550@gmail.com>
2023-06-04 17:52:32 +0300

    feat(handlers): add show reset password page

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/alpden550/go-ecommerce-stripe/internal/encryption"
 	"github.com/alpden550/go-ecommerce-stripe/internal/renders"
+	"github.com/alpden550/go-ecommerce-stripe/internal/urlsigner"
 	"net/http"
 )
 
@@ -46,6 +48,40 @@ func Logout(writer http.ResponseWriter, request *http.Request) {
 func ForgotPassword(writer http.ResponseWriter, request *http.Request) {
 	if err := renders.RenderTemplate(
 		writer, request, "forgot-password", &renders.TemplateData{}, "nav",
+	); err != nil {
+		app.ErrorLog.Printf("%e", fmt.Errorf("%w", err))
+	}
+}
+
+func ShowResetPassword(writer http.ResponseWriter, request *http.Request) {
+	url := request.RequestURI
+	testUrl := fmt.Sprintf("%s%s", app.Config.FrontEnd, url)
+	email := request.URL.Query().Get("email")
+
+	signer := urlsigner.Signer{Secret: []byte(app.Config.SecretKey)}
+	verified := signer.VerifyToken(testUrl)
+	if !verified {
+		app.ErrorLog.Println("invalid hash for email ", email)
+		http.Redirect(writer, request, "/", http.StatusSeeOther)
+	}
+
+	if expired := signer.Expired(testUrl, 60); expired {
+		app.ErrorLog.Println("link expired for email ", email)
+		http.Redirect(writer, request, "/", http.StatusSeeOther)
+	}
+
+	encryptor := encryption.Encryption{Key: []byte(app.Config.SecretKey)}
+	encrEmail, err := encryptor.Encrypt(email)
+	if err != nil {
+		app.ErrorLog.Printf("%w", fmt.Errorf("%e", err))
+		http.Redirect(writer, request, "/", http.StatusSeeOther)
+	}
+
+	data := map[string]interface{}{
+		"email": encrEmail,
+	}
+	if err := renders.RenderTemplate(
+		writer, request, "reset-password", &renders.TemplateData{Data: data}, "nav",
 	); err != nil {
 		app.ErrorLog.Printf("%e", fmt.Errorf("%w", err))
 	}
