1caeb7c6430fdf6fd1b92fc54d5253a26e5077ba
Denis Novikov <alpden550@gmail.com>
2023-06-05 19:34:45 +0300

    feat(models): add insert subscription order

--- internal/models/order.go
+++ internal/models/order.go
@@ -12,15 +12,16 @@ type OrderStatus struct {
 }
 
 type Order struct {
-	ID            int       `json:"id"`
-	WidgetID      int       `json:"widget_id"`
-	TransactionID int       `json:"transaction_id"`
-	CustomerID    int       `json:"customer_id"`
-	StatusID      int       `json:"status_id"`
-	Quantity      int       `json:"quantity"`
-	Amount        int       `json:"amount"`
-	CreatedAt     time.Time `json:"-"`
-	UpdatedAt     time.Time `json:"-"`
+	ID             int       `json:"id"`
+	WidgetID       int       `json:"widget_id"`
+	SubscriptionID int       `json:"subscription_id"`
+	TransactionID  int       `json:"transaction_id"`
+	CustomerID     int       `json:"customer_id"`
+	StatusID       int       `json:"status_id"`
+	Quantity       int       `json:"quantity"`
+	Amount         int       `json:"amount"`
+	CreatedAt      time.Time `json:"-"`
+	UpdatedAt      time.Time `json:"-"`
 }
 
 func (m *DBModel) InsertOrder(order Order) (int, error) {
@@ -34,6 +35,25 @@ func (m *DBModel) InsertOrder(order Order) (int, error) {
 
 	err := m.DB.QueryRow(
 		query, order.WidgetID, order.TransactionID, order.CustomerID, order.StatusID, order.Quantity, order.Amount,
+	).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (m *DBModel) InsertSubscriptionOrder(order Order) (int, error) {
+	var id int
+
+	query := `
+		INSERT INTO orders (subscription_id, transaction_id, customer_id, status_id, quantity, amount)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id
+	`
+
+	err := m.DB.QueryRow(
+		query, order.SubscriptionID, order.TransactionID, order.CustomerID, order.StatusID, order.Quantity, order.Amount,
 	).Scan(&id)
 	if err != nil {
 		return 0, err
