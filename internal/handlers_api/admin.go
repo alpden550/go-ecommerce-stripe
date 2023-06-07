e2f8921ad2c7a3a3cfc35b8c79c376dc43865932
Denis Novikov <alpden550@gmail.com>
2023-06-07 18:17:49 +0300

    feat(handlers api): add get widget orders handler

---
+++ internal/handlers_api/admin.go
@@ -0,0 +1,20 @@
+package handlers_api
+
+import (
+	"github.com/alpden550/go-ecommerce-stripe/internal/helpers"
+	"net/http"
+)
+
+func AllSales(writer http.ResponseWriter, request *http.Request) {
+	sales, err := helpers.FetchAllWidgetOrder(api)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	err = helpers.WriteJSON(api, writer, http.StatusOK, sales)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+}
