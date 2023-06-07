3a41b13b561de5106dfc2308fac4f1a91b1dcee1
Denis Novikov <alpden550@gmail.com>
2023-06-07 21:04:26 +0300

    feat(handlers): add getting all subscriptions handler

--- internal/handlers_api/admin.go
+++ internal/handlers_api/admin.go
@@ -18,3 +18,17 @@ func AllSales(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 }
+
+func AllSubscriptions(writer http.ResponseWriter, request *http.Request) {
+	sales, err := helpers.FetchAllSubscriptionsOrder(api)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	err = helpers.WriteJSON(api, writer, http.StatusOK, sales)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+}
