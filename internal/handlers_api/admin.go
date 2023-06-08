abedd6a72f0bc8ba961785024e891ec5ba8310cc
Denis Novikov <alpden550@gmail.com>
2023-06-08 16:36:49 +0300

    feat(handlers api): add orders detail api handlers

--- internal/handlers_api/admin.go
+++ internal/handlers_api/admin.go
@@ -2,7 +2,9 @@ package handlers_api
 
 import (
 	"github.com/alpden550/go-ecommerce-stripe/internal/helpers"
+	"github.com/go-chi/chi/v5"
 	"net/http"
+	"strconv"
 )
 
 func AllSales(writer http.ResponseWriter, request *http.Request) {
@@ -19,6 +21,26 @@ func AllSales(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func GetSale(writer http.ResponseWriter, request *http.Request) {
+	orderId, err := strconv.Atoi(chi.URLParam(request, "id"))
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	order, err := helpers.GetWidgetOrder(api, orderId)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	err = helpers.WriteJSON(api, writer, http.StatusOK, order)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+}
+
 func AllSubscriptions(writer http.ResponseWriter, request *http.Request) {
 	sales, err := helpers.FetchAllSubscriptionsOrder(api)
 	if err != nil {
@@ -27,6 +49,26 @@ func AllSubscriptions(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	err = helpers.WriteJSON(api, writer, http.StatusOK, sales)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+}
+
+func GetSubscription(writer http.ResponseWriter, request *http.Request) {
+	orderId, err := strconv.Atoi(chi.URLParam(request, "id"))
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	order, err := helpers.GetSubscriptionOrder(api, orderId)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	err = helpers.WriteJSON(api, writer, http.StatusOK, order)
 	if err != nil {
 		helpers.BadRequest(api, writer, request, err)
 		return
