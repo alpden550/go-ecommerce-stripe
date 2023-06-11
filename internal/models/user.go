7441e470a87c0f33b244e4aa9ac1f7c7bdb665d7
Denis Novikov <alpden550@gmail.com>
2023-06-11 15:59:27 +0300

    fix(models): return created at for user in json

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,7 +12,7 @@ type User struct {
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
 	Password  string    `json:"-"`
-	CreatedAt time.Time `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"-"`
 }
 
