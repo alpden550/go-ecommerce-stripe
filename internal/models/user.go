c3dac922a37dbea2605ad445c4b733ee657f1f5d
Denis Novikov <alpden550@gmail.com>
2023-06-12 13:56:24 +0300

    fix(models): make password readable from json

--- internal/models/user.go
+++ internal/models/user.go
@@ -11,7 +11,7 @@ type User struct {
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"-"`
 }
