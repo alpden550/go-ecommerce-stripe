0e0bb94de2c5fc4ca9975ee09ef6e7082c9b1edc
Denis Novikov <alpden550@gmail.com>
2023-06-12 13:56:55 +0300

    feat(handlers api): add editing user

--- internal/handlers_api/user.go
+++ internal/handlers_api/user.go
@@ -1,8 +1,13 @@
 package handlers_api
 
 import (
+	"fmt"
 	"github.com/alpden550/go-ecommerce-stripe/internal/helpers"
+	"github.com/alpden550/go-ecommerce-stripe/internal/models"
+	"github.com/go-chi/chi/v5"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"strconv"
 )
 
 func AllUsers(writer http.ResponseWriter, request *http.Request) {
@@ -16,4 +21,85 @@ func AllUsers(writer http.ResponseWriter, request *http.Request) {
 	if err != nil {
 		helpers.BadRequest(api, writer, request, err)
 	}
+}
+
+func OneUser(writer http.ResponseWriter, request *http.Request) {
+	id := chi.URLParam(request, "id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	user, err := helpers.FetchUserByID(api, userID)
+	if err != nil {
+		helpers.WriteJSON(api, writer, http.StatusNotFound, err)
+		return
+	}
+
+	err = helpers.WriteJSON(api, writer, http.StatusOK, user)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+	}
+}
+
+func EditUser(writer http.ResponseWriter, request *http.Request) {
+	id := chi.URLParam(request, "id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	var user models.User
+	err = helpers.ReadJSON(api, writer, request, &user)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+		return
+	}
+
+	response := jsonResponse{
+		OK: true,
+	}
+
+	if userID > 0 {
+		err = helpers.EditUser(api, &user)
+		if err != nil {
+			helpers.BadRequest(api, writer, request, err)
+			return
+		}
+
+		if user.Password != "" {
+			newHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
+			if err != nil {
+				helpers.BadRequest(api, writer, request, err)
+				return
+			}
+
+			err = helpers.UpdateUserPassword(api, &user, string(newHash))
+			if err != nil {
+				helpers.BadRequest(api, writer, request, err)
+				return
+			}
+		}
+		response.Message = fmt.Sprintf("Updated user %d", userID)
+	} else {
+		newHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
+		if err != nil {
+			helpers.BadRequest(api, writer, request, err)
+			return
+		}
+		newUserId, err := helpers.SaveUser(api, &user, string(newHash))
+		if err != nil {
+			helpers.BadRequest(api, writer, request, err)
+			return
+		}
+		response.Message = fmt.Sprintf("Added user %d", newUserId)
+	}
+
+	err = helpers.WriteJSON(api, writer, http.StatusOK, response)
+	if err != nil {
+		helpers.BadRequest(api, writer, request, err)
+	}
+
 }
