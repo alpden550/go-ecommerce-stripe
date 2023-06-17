f3d2c759c12069decb377890acaa30a3b125057c
Denis Novikov <alpden550@gmail.com>
2023-06-17 16:06:46 +0300

    feat(interfaces): add base configer

--- internal/configs/interfaces.go
+++ internal/configs/interfaces.go
@@ -1,7 +1,16 @@
 package configs
 
-import "github.com/alpden550/go-ecommerce-stripe/internal/models"
+import (
+	"github.com/alpden550/go-ecommerce-stripe/internal/models"
+	"log"
+)
 
 type AppConfiger interface {
 	GetDB() models.DBModel
+}
+
+type BaseConfiger interface {
+	GetConfig() *Config
+	GetInfoLog() *log.Logger
+	GetErrorLog() *log.Logger
 }
